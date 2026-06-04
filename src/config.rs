20c6f2dc89643711831d222ffa6dcba1e128e3f0
Tan-Micky <916949469@qq.com>
2026-06-04 14:51:33 +0800

    修改配置-Micky

--- src/config.rs
+++ src/config.rs
@@ -117,8 +117,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["10.240.2.35"];
+pub const RS_PUB_KEY: &str = "ZtfftEFjvgkTKIgaeBoIdrpLcR0kLiIVY+b5CdEGpMM=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
