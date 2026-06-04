b1f77e3e7fa91fa691c2866950d867d2ab5c5428
AJIA-CC <xiaohanlike@outlook.com>
2026-06-04 10:35:23 +0800

    config.rs

--- src/config.rs
+++ src/config.rs
@@ -117,8 +117,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["192.168.192.51"];
+pub const RS_PUB_KEY: &str = "ugeuTYVkHKXEEQPA29tthYyMgnVNk0X+p7KSoQ3PgP0=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
