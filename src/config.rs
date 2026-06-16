bb7870cd1139c167ba1ad4e9f2eae8f4e56fb2a4
andrelucasarantonio-sys <andrelucas.ar.antonio@gmail.com>
2026-06-16 16:08:16 -0300

    Update rendezvous server and public key constants

--- src/config.rs
+++ src/config.rs
@@ -117,8 +117,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["rd.consumerapis.com"];
+pub const RS_PUB_KEY: &str = "IVMELcuP5RVgRoxlucAsuTwYOoO00VslrBvN0p68Zp8=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
